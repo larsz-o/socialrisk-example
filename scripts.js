5d22b8e5ee7440b2b38551317d21d671e5dc6213
Lars <larsmackenzie@gmail.com>
2020-01-28 14:31:38 -0500

    add split method to search through array

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
-const postsToAnalyze = [{ body: '...', riskRating: 10}, { body: '...', riskRating: 0 }];
+const postsToAnalyze = [{ body: 'immigration', riskRating: 0}, { body: 'immigration invasion', riskRating: 0 }];
 
 function isThisDangerous(posts) {
     // posts is an array of social media posts, each of which is an object. we'll iterate through the array to look at its contents
     for (let i = 0; i < posts.length; i++) {
-        // let's name the values of each object in a variable called data
-        let data = Object.values(posts[i].body);
-        // if the object contains the word invasion anywhere in it, then execute this code
-        if (data.indexOf('invasion') !== -1) {
+        // let's name the values of each object, within the body key, in a variable called data
+        let data = posts[i].body;
+        // we need to split this string into an array that we can serach through, by splitting at all spaces. 
+        let words = data.split(' ');
+        // next, we search through our new array, called words, to try to find our risk words using the method indexOf
+        if (words.indexOf('invasion') !== -1) {
             // add one to the existing riskRating for this post 
             posts[i].riskRating += 1;
-            //if the object contains the word invasion and also the word immigration anywhere in it, then execute this code
-        } else if (data.indexOf('invasion') !== -1 && data.indexOf('immigration') !== -1) {
+            //if the post contains the word invasion and also the word immigration anywhere in it, then execute this code
+        } else if (words.indexOf('invasion') !== -1 && words.indexOf('immigration') !== -1) {
             // add two to the existing riskRating for this post 
             posts[i].riskRating += 2;
         }
@@ -21,7 +23,6 @@ function isThisDangerous(posts) {
 // sort the data so that the riskiest posts are listed first.
 function sortPostsByRiskRanking(posts) {
     let sorted = posts.sort((a, b) => (b.riskRating > a.riskRating) ? 1 : -1);
-    console.log(sorted);
     return sorted;
 }
 
